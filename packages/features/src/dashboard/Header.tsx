a9296c2e2d4b4677dca68d814c399bba943e695b
Zakariaa Enajjachi <enajjachi.zakariaa@gmail.com>
2025-07-19 20:41:02 +0100

    enhancement: header package implemented

--- packages/features/src/dashboard/Header.tsx
+++ packages/features/src/dashboard/Header.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import { useRole, Can } from "~/packages/acl/src";
 
 export function Header() {
-  // Only render if user can read dashboard
   return (
     <Can I="read" a="dashboard" fallback={null}>
       <p>
